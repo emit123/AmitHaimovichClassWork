d998d9e0913985e7cee62ad6a20c185c259d3962
Amit Haimovich <amithaimovich504@gmail.com>
2024-12-12 20:00:54 +0200

    first commit

---
+++ Password/Password.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+struct Password
+{
+	char value[16];
+	bool incorrect;
+	Password() : value(""), incorrect(true)
+	{
+	}
+};
+
+int main()
+{
+	std::cout << "Enter your password to continue:" << std::endl;
+	Password pwd;
+	std::cin >> pwd.value;
+
+	if (!strcmp(pwd.value, "********"))
+		pwd.incorrect = false;
+
+	if(!pwd.incorrect)
+		std::cout << "Congratulations\n";
+
+	return 0;
+
+}
+/*
+כל קטל בעל 16 תווים כמו:
+****************
+יש באפראוברפלו. הקלט בעצם כותב גם על המשתנה הבוליאני והופך אותו לטרו
+לכן מודפסת הצלחה
+*/
